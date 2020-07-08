999874c7e376ce98d31776c1ef7608cba556a5aa
Ben Bryant <brbryant2639@gmail.com>
2020-07-08 12:07:01 -0500

    Worked on QuestionCard component.

--- src/client/views/Application.tsx
+++ src/client/views/Application.tsx
@@ -1,9 +1,26 @@
 import * as React from "react";
+import QuestionCard from "../components/QuestionCard";
 
 const Application: React.FC<IApplicationProps> = () => {
   return (
     <main className="container my-5">
       <h1 className="text-primary text-center">Application Questions</h1>
+      <QuestionCard
+        QuestionNumber={1}
+        Question={"First Name"}
+        answerType={"text"}
+      />
+      <QuestionCard
+        QuestionNumber={2}
+        Question={"Last Name"}
+        answerType={"text"}
+      />
+      <QuestionCard QuestionNumber={3} Question={"Age"} answerType={"number"} />
+      <QuestionCard
+        QuestionNumber={4}
+        Question={"Why are you interested in software development?"}
+        answerType={"text"}
+      />
     </main>
   );
 };
