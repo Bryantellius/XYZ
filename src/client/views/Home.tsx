c265e80802b87daf88c2338700627662e9a1fcc2
Ben Bryant <brbryant2639@gmail.com>
2020-07-10 13:20:55 -0500

    Added dark mode. Started big graphic.

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import DashboardGraph from "../components/DashboardGraph";
+import TotalGraphic from "../components/TotalGraphic";
 
 const Home: React.FC<IHomeProps> = () => {
   return (
-    <main className="container my-5">
-      <h1 className="text-dark text-center p-2 border-bottom border-primary">TrueCoders Dashboard</h1>
+    <main className="container mb-5">
+      <h1 className="text-light text-center p-2 border-bottom border-primary">TrueCoders Dashboard</h1>
+      <TotalGraphic />
       <DashboardGraph />
     </main>
   );
