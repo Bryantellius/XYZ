0290554d618536ae156892bfd1fb09032fa52492
Ben Bryant <brbryant2639@gmail.com>
2020-07-31 10:59:59 -0500

    Edited format. Added 6 promo spots

--- src/client/views/Home.tsx
+++ src/client/views/Home.tsx
@@ -15,7 +15,7 @@ const Home: React.FC<IHomeProps> = () => {
           </div>
           <div>
             <a href="_blank" className="text-dark h5">
-              Learn More >
+              Learn More {">"}
             </a>
           </div>
         </div>
@@ -33,7 +33,7 @@ const Home: React.FC<IHomeProps> = () => {
           </div>
           <div>
             <a href="_blank" className="text-dark h5">
-              Learn More >
+              Learn More {">"}
             </a>
           </div>
         </div>
@@ -42,7 +42,8 @@ const Home: React.FC<IHomeProps> = () => {
       <div id="homeOnePlusEightProInfo">
         <div id="HOPEPI">
           <div className="mb-5 h1">
-            OnePlus 8 Pro<br />
+            OnePlus 8 Pro
+            <br />
             Lead with Speed
           </div>
           <div className="h3">$899 or $39/mo with 0% APR</div>
@@ -51,13 +52,38 @@ const Home: React.FC<IHomeProps> = () => {
           </div>
           <div>
             <a href="_blank" className="text-dark h5">
-              Learn More >
+              Learn More {">"}
             </a>
           </div>
         </div>
       </div>
 
-      
+      <div id="nextFourOnHome" className="container.fluid">
+        <div className="row">
+          <div className="homeFour col-md-6 bg-primary">
+            <a href="" className="nav-link text-dark h1">Check Me Out</a>
+          </div>
+          <div className="homeFour col-md-6 bg-success">
+            <a href="" className="nav-link text-dark h1">Check Me Out</a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="homeFour col-md-6 bg-danger">
+            <a href="" className="nav-link text-dark h1">Check Me Out</a>
+          </div>
+          <div className="homeFour col-md-6 bg-info">
+            <a href="" className="nav-link text-dark h1">Check Me Out</a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="homeFour col-md-6 bg-light shadow-lg">
+            <a href="" className="nav-link text-dark h1">Check Me Out</a>
+          </div>
+          <div className="homeFour col-md-6 bg-dark">
+            <a href="" className="nav-link text-light h1">Check Me Out</a>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
