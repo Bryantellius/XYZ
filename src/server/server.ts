e2c6aa28052a74d5217b224d9281b0634ed5ddf9
Ben Bryant <brbryant2639@gmail.com>
2020-08-06 14:06:58 -0500

    Fixed express.static

--- src/server/server.ts
+++ src/server/server.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.use(morgan("dev"));
